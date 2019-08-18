2a8fd9fdf71b62fe3d9f729afd0ad795d8706227
dhiegohenrique <dhiego_henrique20@yahoo.com.br>
2019-08-17 21:43:36 -0300

    add spaces

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 module.change_code = 1;
 'use strict';
 
-const alexa = require( 'alexa-app' );
-const app = new alexa.app( 'myskill' );
+const alexa = require('alexa-app');
+const app = new alexa.app('myskill');
 
 
 app.launch((request, response) => {
-        response.say( 'Welcome to Alexa World' ).reprompt( 'Ask Something.' ).shouldEndSession( false );
-} );
+  response.say('Welcome to Alexa World').reprompt('Ask Something.').shouldEndSession(false);
+});
 
 
 app.error = (error, request, response) => {
- response.say( 'Sorry some error occured ' + error.message);
+  response.say('Sorry some error occured ' + error.message);
 };
 
 app.intent('sayHello',
   {
- "utterances":[ 
-  "say Hello",
-  "Hello alexa",
-  "What's up",
-  "Hey alexa"]
+    "utterances": [
+      "say Hello",
+      "Hello alexa",
+      "What's up",
+      "Hey alexa"]
   },
-  (request,response) => {
+  (request, response) => {
     response.say("Hello, Welcome to alexa world.");
   }
 );
