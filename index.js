42633fb27dd35457e1b6e4ed89a82c92a925ce4c
dhiegohenrique <dhiego_henrique20@yahoo.com.br>
2019-08-18 17:15:37 -0300

    add moment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.change_code = 1
 
 const alexa = require('alexa-app')
 const app = new alexa.app('alexa-cine-skill')
-// const moment = require('moment')
+const moment = require('moment')
 const cheerio = require('cheerio')
 const axios = require('axios')
 const numeroPorExtenso = require('numero-por-extenso')
@@ -47,39 +47,18 @@ app.intent('CheckStatusIntent',
   },
   (req, res) => {
     let url = 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22'
-    url = 'http://www.adorocinema.com/filmes/agenda/mes/mes-2019-07/'
+
+    const currentDate = moment()
+    const month = currentDate.month()
+    const year = currentDate.year()
+    url = `http://www.adorocinema.com/filmes/agenda/mes/mes-${year}-${month}/`
 
     return new Promise(async (resolve) => {
-      // não funcionou:
-      // const response = await request.get(url)
-      // const body = JSON.parse(response.body)
-      // console.log('body.weather.description: ' + body.name)
-      // res.say('esta é a resposta: ' + body.name)
-      // resolve()
-
-      // request.get(url, (error, response, body) => {
-      //   body = JSON.parse(body)
-      //   console.log('body.weather.description: ' + body.name)
-      //   res.say('esta é a resposta: ' + body.name)
-      //   resolve()
-      // })
-
-      // const currentDate = moment()
-      // const month = currentDate.month()
-      // const year = currentDate.year()
-      // res.say(`o mês atual é ${month} e o ano atual é ${year}`)
-      // resolve()
-
       const response = await axios.get(url)
       const $ = cheerio.load(response.data)
 
       const arrayReleases = []
-
-      const mapMovies = new Map()
       $('.movie-agenda-month').each((indexAgenda, el) => {
-        //lançamento1
-        mapMovies.set(indexAgenda, '')
-
         let releaseDate = $('.title-inter', el).first().text().trim()
         releaseDate = releaseDate.replace('Estreias de ', '')
 
@@ -102,11 +81,11 @@ app.intent('CheckStatusIntent',
       })
 
       arrayReleases.forEach((release) => {
-        var speech = new Speech()
+        let speech = new Speech()
           .say(release.releaseDate)
           .pause('2s')
 
-        var speechOutput = speech.ssml(true)
+        let speechOutput = speech.ssml(true)
         res.say(speechOutput)
 
         release.movies.forEach((movie) => {
@@ -118,24 +97,6 @@ app.intent('CheckStatusIntent',
           res.say(speechOutput)
         })
       })
-
-      // $('.title-inter').each((index, el) => {
-      //   let title = $(el).text().trim()
-      //   title = title.replace('Estreias de ', '')
-
-      //   let array = title.split(' ')
-      //   array[0] = numeroPorExtenso.porExtenso(array[0])
-      //   array[array.length - 1] = numeroPorExtenso.porExtenso(array[array.length - 1])
-
-      //   title = `Estréias de ${array.join(' ')}`
-
-      //   var speech = new Speech()
-      //     .say(title)
-      //     .pause('2s')
-
-      //   var speechOutput = speech.ssml(true)
-      //   res.say(speechOutput)
-      // })
 
       resolve()
     })
