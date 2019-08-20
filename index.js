9ebfe8f4323d0875fc622107f2139f6c6ed63c77
dhiego <dhiego@portaltelemedicina.com.br>
2019-08-20 10:33:25 -0300

    add sayReleases

--- index.js
+++ index.js
@@ -11,35 +11,16 @@ const months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'jul
 
 app.launch((request, response) => {
   response
-    .say('Bem-vindo a programação do cinema')
-    .reprompt('Qual programação você deseja saber?')
+    .say('Bem-vindo aos lançamentos do cinema')
+    .reprompt('Você deseja saber os lançamentos deste mês ou desta semana?')
     .shouldEndSession(false)
 })
 
 app.error = (error, request, response) => {
+  console.error(`Erro: ${error.message}`)
   response
-    .say('Desculpe, algum erro ocorreu: ' + error.message)
+    .say(`Desculpe, algum erro ocorreu: ${error.message}`)
 }
-
-app.intent('MonthlyScheduleIntent',
-  {
-    'utterances': [
-      'deste mês']
-  },
-  (request, response) => {
-    response.say('Digo deste mês')
-  }
-)
-
-app.intent('WeeklyScheduleIntent',
-  {
-    'utterances': [
-      'desta semana']
-  },
-  (request, response) => {
-    response.say('Digo da semana')
-  }
-)
 
 const getMonthlySchedule = () => {
   return new Promise(async (resolve) => {
@@ -47,43 +28,44 @@ const getMonthlySchedule = () => {
     let month = parseInt(currentDate.month())
     month = month + 1
 
-    if (parseInt(month) < 10 && !month.toString().startsWith('0')) {
+    if (month < 10 && !month.toString().startsWith('0')) {
       month = `0${month}`
     }
 
     const year = currentDate.year()
     const url = `http://www.adorocinema.com/filmes/agenda/mes/mes-${year}-${month}/`
 
-    const response = await axios.get(url)
-    const $ = cheerio.load(response.data)
+    const res = await axios.get(url)
+    const $ = cheerio.load(res.data)
 
     const arrayReleases = []
     $('.movie-agenda-month').each((index, el) => {
       let releaseDate = $('.title-inter', el).first().text().trim()
       releaseDate = releaseDate.replace('Estreias de ', '')
-      let array = releaseDate.split(' ')
-      array = array.filter((item) => {
+      
+      let arrayDate = releaseDate.split(' ')
+      arrayDate = arrayDate.filter((item) => {
         return item !== 'de'
       })
 
       let monthIndex = months.findIndex((month) => {
-        return month.toLowerCase() === array[1].toLowerCase()
+        return month.toLowerCase() === arrayDate[1].toLowerCase()
       })
       monthIndex = monthIndex + 1
-      array[1] = `${monthIndex}`
-      releaseDate = array.join('/')
+      arrayDate[1] = `${monthIndex}`
+      releaseDate = arrayDate.join('/')
 
-      const obj = {
+      const objRelease = {
         releaseDate,
         movies: []
       }
 
       $('.month-movies-link', el).each((index, el) => {
         const movie = $(el).text().trim()
-        obj.movies.push(movie)
+        objRelease.movies.push(movie)
       })
 
-      arrayReleases.push(obj)
+      arrayReleases.push(objRelease)
     })
 
     resolve(arrayReleases)
@@ -92,7 +74,7 @@ const getMonthlySchedule = () => {
 
 const formatDate = (arrayReleases) => {
   return arrayReleases.map((release) => {
-    let releaseDate = release.releaseDate
+    const releaseDate = release.releaseDate
     let arrayDate = releaseDate.split('/')
 
     arrayDate[0] = numero.porExtenso(arrayDate[0])
@@ -107,80 +89,44 @@ const formatDate = (arrayReleases) => {
 const getWeeklySchedule = () => {
   return new Promise(async (resolve) => {
     const currentDate = moment()
-    const year = currentDate.year()
-    const day = currentDate.day()
-
-    let month = parseInt(currentDate.month())
-    month = month + 1
-
-    if (parseInt(month) < 10 && !month.toString().startsWith('0')) {
-      month = `0${month}`
-    }
-
-    let agora = moment();
-
-    let inicio = moment().day(0); // domingo desta semana
-    let fim = moment().day(6); // sábado desta semana
-
-    // imprimir as datas no formato desejado
-    let formato = 'DD/MM/YYYY';
-    console.log('agora=', agora.format(formato));
-    console.log('início=', inicio.format(formato));
-    console.log('fim=', fim.format(formato));
+    const startDate = currentDate.day(0)
+    const endDate = currentDate.day(6)
 
     let arrayReleases = await getMonthlySchedule()
-    // let arrayWeekly = []
-    // for (let index = 0;index < arrayReleases.length;index++) {
-    //   const release = arrayReleases[index]
-    //   let date = moment(release.releaseDate, `DD/MM/YYYY`)
-    //   if (date.isBetween(inicio, fim)) {
-    //     arrayWeekly.push(release)
-    //   }
-
-    //   if (date.isAfter(fim)) {
-    //     break
-    //   }
-    // }
-
     arrayReleases = arrayReleases.filter((release) => {
       let date = moment(release.releaseDate, `DD/MM/YYYY`)
-      return date.isBetween(inicio, fim)
+      return date.isBetween(startDate, endDate)
     })
 
-    // arrayReleases = arrayWeekly
-    // console.log('arrayReleases: ' + JSON.stringify(arrayReleases))
     resolve(arrayReleases)
   })
 }
 
-app.intent('CheckStatusIntent',
+app.intent('WeeklyScheduleIntent',
   {
     'utterances': [
-      'deste site']
+      'desta semana']
   },
   (req, res) => {
     return new Promise(async (resolve) => {
       let arrayReleases = await getWeeklySchedule()
       arrayReleases = formatDate(arrayReleases)
-      // const arrayReleases = await getMonthlySchedule()
-      arrayReleases.forEach((release) => {
-        let speech = new Speech()
-          .say(release.releaseDate)
-          .pause('2s')
+      await sayReleases(res, arrayReleases)
+      resolve()
+    })
+  }
+)
 
-        let speechOutput = speech.ssml(true)
-        res.say(speechOutput)
-
-        release.movies.forEach((movie) => {
-          speech = new Speech()
-            .say(movie)
-            .pause('1s')
-
-          speechOutput = speech.ssml(true)
-          res.say(speechOutput)
-        })
-      })
-
+app.intent('MonthlyScheduleIntent',
+  {
+    'utterances': [
+      'deste mês']
+  },
+  (req, res) => {
+    return new Promise(async (resolve) => {
+      let arrayReleases = await getMonthlySchedule()
+      arrayReleases = formatDate(arrayReleases)
+      await sayReleases(res, arrayReleases)
       resolve()
     })
   }
@@ -191,11 +137,38 @@ app.intent('AMAZON.HelpIntent', {
   'utterances': []
 },
   (request, response) => {
-    var helpOutput = 'Você pode escolher entre a programação do mês ou da semana'
-    var reprompt = 'Qual programação você deseja saber?'
+    const helpOutput = 'Você pode escolher entre a programação deste mês ou desta semana'
+    const reprompt = 'Qual programação você deseja saber?'
     // AMAZON.HelpIntent must leave session open -> .shouldEndSession(false)
-    response.say(helpOutput).reprompt(reprompt).shouldEndSession(false)
+    response
+      .say(helpOutput)
+      .reprompt(reprompt)
+      .shouldEndSession(false)
   }
 )
+
+const sayReleases = (res, arrayReleases) => {
+  return new Promise((resolve) => {
+    arrayReleases.forEach((release) => {
+      let speech = new Speech()
+        .say(release.releaseDate)
+        .pause('2s')
+
+      let speechOutput = speech.ssml(true)
+      res.say(speechOutput)
+
+      release.movies.forEach((movie) => {
+        speech = new Speech()
+          .say(movie)
+          .pause('1s')
+
+        speechOutput = speech.ssml(true)
+        res.say(speechOutput)
+      })
+    })
+
+    resolve()
+  })
+}
 
 module.exports = app
